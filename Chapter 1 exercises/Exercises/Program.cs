319e0dd7b600634d8de45328f7fc384c00c1cfc1
EmilGrouleff <emil-github@grouleff.com>
2021-01-17 02:08:26 +0100

    Chapter 1 exercises, and some Chapter 2 exercises

---
+++ Chapter 1 exercises/Exercises/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ex11();
+        }
+
+        //Write a console application that prints your first and last name on the console.
+        static void ex6()
+        {
+            Console.WriteLine("Emil Grouleff");
+        }
+
+        //Write a program that prints the following numbers on the console 1, 101, 1001, each on a new line.
+        static void ex7()
+        {
+            Console.WriteLine("1");
+            Console.WriteLine("101");
+            Console.WriteLine("1001");
+        }
+
+        //Write a program that prints on the console the current date and time.
+        static void ex8()
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+
+        //Write a program that prints the square root of 12345.
+        static void ex9()
+        {
+            Console.WriteLine(Math.Sqrt(12345));
+        }
+
+        //Write a program that prints the first 100 members of the sequence 2, -3, 4, -5, 6, -7, 8.
+        static void ex10()
+        {
+            bool minus = true;
+            for (int i = 2; i < 103; i++)
+            {
+                if (minus)
+                {
+                    Console.WriteLine(i);
+                    minus = false;
+                }
+                else
+                {
+                    Console.WriteLine("-"+i);
+                    minus = true;
+                }
+            }
+        }
+
+        //Write a program that reads your age from the console and prints your age after 10 years.
+        static void ex11()
+        {
+            int age;
+            Console.WriteLine("Please enter your age!");
+            age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your age in 10 years is: " + (age + 10));
+        }
+
+        //
+
+
+    }
+}
---
+++ Chapter 2 exercises/Exercises/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ex5();
+        }
+
+        /*
+        Declare several variables by selecting for each one of them the most
+        appropriate of the types sbyte, byte, short, ushort, int, uint, long
+        and ulong in order to assign them the following values: 52,130; -115;
+        4825932; 97; -10000; 20000; 224; 970,700,000; 112; -44; -1,000,000;
+        1990; 123456789123456789.
+        */
+        static void Ex1()
+        {
+            /*
+            sbyte a = -115;
+            sbyte a2 = 97;
+            sbyte a3 = 112;
+            sbyte a4 = -44;
+
+            byte b = 224;
+            
+            short c = -10000;
+            short c2 = 20000;
+            short c3 = 1990;
+            
+            ushort d = 52130;
+            
+            int e = 4825932;
+            int e2 = -1000000;
+            int e3 = 970700000;
+
+            long g = 123456789123456789;
+            */
+        }
+
+        /*
+        Which of the following values can be assigned to variables of type float,
+        double and decimal: 5, -5.01, 34.567839023; 12.345; 8923.1234857;
+        3456.091124875956542151256683467?
+        */
+        static void Ex2()
+        {
+            //float: -5.01; 12.345; 5
+            //Double: 3456.091124875956542151256683467 
+            //Decimal: 34.567839023; 8923.1234857
+        }
+
+        /*
+        Write a program, which compares correctly two real numbers with
+        accuracy at least 0.000001.
+        */
+        static void Ex3()
+        {
+            float numberOne;
+            float numberTwo;
+
+            Console.WriteLine("Write a number");
+            numberOne = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Write a second number");
+            numberTwo = float.Parse(Console.ReadLine());
+
+            if (numberOne==numberTwo)
+            {
+                Console.WriteLine("The numbers are the same");
+            }
+            else
+            {
+                Console.WriteLine("The numbers ain't the same");
+            }
+        }
+
+        /*
+        Initialize a variable of type int with a value of 256 in
+        hexadecimal format (256 is 100 in a numeral system with base 16).
+        */
+        static void Ex4()
+        {
+            int numberInInt = 256;
+            int numberInHex = 0x100;
+
+            Console.WriteLine(numberInInt);
+            Console.WriteLine(numberInHex);
+        }
+
+        /*
+        Declare a variable of type char and assign it as a value the character,
+        which has Unicode code, 72 (use the Windows calculator in order to find
+        hexadecimal representation of 72). 
+        */
+        static void Ex5()
+        {
+            char MyChar = '\u0048';
+            Console.WriteLine(MyChar);
+        }
+    }
+}
