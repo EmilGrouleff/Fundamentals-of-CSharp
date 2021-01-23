e68d94ef9da8387e95064c8b5855481503c131c3
EmilGrouleff <emil-github@grouleff.com>
2021-01-23 17:47:08 +0100

    Chapter 3 exercise up to 11

--- Chapter 3 exercises/Exercises chapter 3/Program.cs
+++ Chapter 3 exercises/Exercises chapter 3/Program.cs
@@ -6,7 +6,7 @@ namespace Exercises_chapter_3
     {
         static void Main()
         {
-            Ex8();
+            Ex10();
         }
 
         //Write an expression that checks whether an integer is odd or even.
@@ -124,10 +124,79 @@ namespace Exercises_chapter_3
         //Write an expression that checks for a given point {x, y} if it is within the circle K({0, 0}, R=5). Explanation: the point {0, 0} is the center of the circle and 5 is the radius.
         static void Ex8()
         {
-            Console.WriteLine("Write a point");
-            float a = float.Parse(Console.ReadLine());
-            Console.WriteLine("Write a second point");
-            float b = float.Parse(Console.ReadLine());
+            Console.WriteLine("Write point x");
+            float x = float.Parse(Console.ReadLine());
+            Console.WriteLine("Write point y");
+            float y = float.Parse(Console.ReadLine());
+
+            if ((x*x)+(y*y)<=5*5)
+            {
+                Console.WriteLine("The point " + x + ", " + y + " is within the circle");
+            }
+            else
+            {
+                Console.WriteLine("it isn't within the circle");
+            }
+        }
+
+        /*
+        Write an expression that checks for given point {x, y} if it is within the circle K({0, 0}, R=5) and out of the rectangle [{-1, 1}, {5, 5}]. 
+        Clarification: for the rectangle the lower left and the upper right corners are given.
+        */
+        static void Ex9()
+        {
+            Console.WriteLine("Write point x");
+            float x = float.Parse(Console.ReadLine());
+            Console.WriteLine("Write point y");
+            float y = float.Parse(Console.ReadLine());
+
+            bool isInCircle = false;
+            bool isInRectangle = false;
+
+            isInCircle = (x * x) + (y * y) <= 5 * 5;
+            isInRectangle = (x > -1 && x < 5) && (y > 1 && y < 5);
+
+
+            if (isInCircle && !isInRectangle)
+            {
+                Console.WriteLine("The point " + x + ", " + y + " is within the circle, and outside the rectangle");
+            }
+            else
+            {
+                Console.WriteLine("it isn't within the circle, and outside the rectangle");
+            }
+        }
+
+        /*Write a program that takes as input a four-digit number in format abcd (e.g. 2011) and performs the following actions:
+        - Calculates the sum of the digits (in our example 2+0+1+1 = 4).
+        - Prints on the console the number in reversed order: dcba (in our example 1102).
+        - Puts the last digit in the first position: dabc (in our example 1201).
+        - Exchanges the second and the third digits: acbd (in our example 2101).*/
+        static void Ex10()
+        {
+            Console.WriteLine("Write a four digit number");
+            int number = int.Parse(Console.ReadLine());
+
+            int d = number % 10;
+            int c = (number / 10) % 10;
+            int b = (number / 100) % 10;
+            int a = (number / 1000) % 10;
+
+            Console.WriteLine("the sum of the numbers are: " + (a+b+c+d));
+            Console.WriteLine("number in reversed order: {3}{2}{1}{0}", a,b,c,d);
+            Console.WriteLine("last digit in the first position: {3}{0}{1}{2}", a,b,c,d);
+            Console.WriteLine("Exchanges the second and the third digits: {0}{2}{1}{3}", a,b,c,d);
+        }
+
+        /* We are given a number n and a position p. Write a sequence of operations that prints the value of the bit on the position p in the number (0 or 1). 
+        Example: n=35, p=5 -> 1. Another example: n=35, p=6 -> 0.*/
+        static void Ex11()
+        {
+            Console.WriteLine("Write number n");
+            float n = float.Parse(Console.ReadLine());
+            Console.WriteLine("Write position p");
+            float p = float.Parse(Console.ReadLine());
+
 
 
 
