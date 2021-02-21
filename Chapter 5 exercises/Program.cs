a70c65ca314087806a002909dfe694aeb133a51a
EmilGrouleff <emil-github@grouleff.com>
2021-02-21 16:52:57 +0100

    Chapter 5 exercise 5-6

--- Chapter 5 exercises/Program.cs
+++ Chapter 5 exercises/Program.cs
@@ -6,7 +6,7 @@ namespace Chapter_5_exercises
     {
         static void Main()
         {
-            Ex4();
+            Ex7();
         }
 
         //Write an if-statement that takes two integer variables and exchanges their values if the first one is greater than the second one.
@@ -115,7 +115,7 @@ namespace Chapter_5_exercises
             float a = float.Parse(Console.ReadLine());
             Console.WriteLine("Write the second real number b");
             float b = float.Parse(Console.ReadLine());
-            Console.WriteLine("Write the thrid real number b");
+            Console.WriteLine("Write the thrid real number c");
             float c = float.Parse(Console.ReadLine());
 
             if (a>b)
@@ -177,11 +177,79 @@ namespace Chapter_5_exercises
         //Write a program that asks for a digit (0-9), and depending on the input, shows the digit as a word (in English). Use a switch statement.
         static void Ex5()
         {
+            Console.WriteLine("Write a digit within the range 0-9");
+            int digit = int.Parse(Console.ReadLine());
+
+            switch (digit)
+            {
+                case 0:
+                    Console.WriteLine("Zero"); break;
+                case 1:
+                    Console.WriteLine("One"); break;
+                case 2:
+                    Console.WriteLine("Two"); break;
+                case 3:
+                    Console.WriteLine("Three"); break;
+                case 4:
+                    Console.WriteLine("Four"); break;
+                case 5:
+                    Console.WriteLine("Five"); break;
+                case 6:
+                    Console.WriteLine("Six"); break;
+                case 7:
+                    Console.WriteLine("Seven"); break;
+                case 8:
+                    Console.WriteLine("Eight"); break;
+                case 9:
+                    Console.WriteLine("Nine"); break;
+                default:
+                    Console.WriteLine("That's not a digit within range 0-9"); break;
+            }
+
+
 
         }
 
+        //Write a program that gets the coefficients a, b and c of a quadratic equation: 
+        //ax2 + bx + c, calculates and prints its real roots (if they exist). Quadratic equations may have 0, 1 or 2 real roots.
+        static void Ex6()
+        {
+            Console.WriteLine("Write the first coefficient a");
+            float a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Write the second coefficient b");
+            float b = float.Parse(Console.ReadLine());
+            Console.WriteLine("Write the thrid coefficient c");
+            float c = float.Parse(Console.ReadLine());
+            double d;
+            double x1;
+            double x2;
+
+            d = b * b - 4 * a * c;
+
+            if (d<0)
+            {
+                Console.WriteLine("There are no root(s)");
+            }
+            else if (d==0)
+            {
+                x1 = -b / (2 * a);
+                Console.WriteLine("There is one root: {0}",x1);
+            }
+            else if (d>0)
+            {
+                x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                Console.WriteLine("There are two roots: {0} and {1}", x1, x2);
+            }
 
 
+        }
+
+        //Write a program that finds the greatest of given 5 numbers.
+        static void Ex7()
+        {
+
+        }
 
     }
 }
